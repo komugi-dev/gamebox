c5d803d1db8c0a1e9927be9c5e764e078158c953
Francesco Formenti <francesco.formenti@nielsen.com>
2026-07-24 17:50:11 +0200

    client player: added turn id and player guid, refactored methods

--- client/player.go
+++ client/player.go
@@ -66,6 +66,7 @@ func (p *Player) join(ctx context.Context, t Table) (string, error) {
 		return "", err
 	}
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
 	resp, err := s.cli.Do(req)
 	if err != nil {
 		return "", err
@@ -109,6 +110,7 @@ func (p *Player) rejoin(ctx context.Context) (string, error) {
 		return "", err
 	}
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
 	resp, err := s.cli.Do(req)
 	if err != nil {
 		return "", err
@@ -150,7 +152,7 @@ func (p *Player) wsConn(ctx context.Context, secret string) error {
 		if resp != nil {
 			status = fmt.Sprintf("%d", resp.StatusCode)
 		}
-		return fmt.Errorf("websocket handshake failed: %v (status: %v)", err, status)
+		return fmt.Errorf("websocket handshake failed: %w (status: %v)", err, status)
 	}
 
 	// get the guid
@@ -159,51 +161,53 @@ func (p *Player) wsConn(ctx context.Context, secret string) error {
 	err = conn.ReadJSON(&welcomeMsg)
 	if err != nil {
 		_ = conn.Close()
-		return fmt.Errorf("failed to read welcome message: %v", err)
+		return fmt.Errorf("failed to read welcome message: %w", err)
 	}
 	_ = conn.SetReadDeadline(time.Time{})
 	p.Guid = welcomeMsg.PlayerGUID
 
 	// run the client
+	if p.cli != nil {
+		p.cli.dispose()
+	}
 	wsCtx, canc := context.WithCancel(context.Background())
 	p.cli = &client{
 		conn:       conn,
 		cancel:     canc,
 		playerGUID: p.Guid,
 	}
-	go func() {
-		err := p.cli.run(wsCtx, p.inbox, p.outbox)
-		if err != nil {
-			log.Errorf("client run error; %v", err)
-		}
-	}()
+	p.cli.run(wsCtx, p.inbox, p.outbox)
 
 	return nil
 
 }
 
 // Join() - The player joins a table and instantiate the web socket.
+// Note: Join, Rejoin and Quit are not thread-safe.
+// Do not call them from different go routines in the same instance of PLayer.
 func (p *Player) Join(ctx context.Context, t Table) error {
 	secret, err := p.join(ctx, t)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to join; %w", err)
 	}
 	err = p.wsConn(ctx, secret)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to establish ws connection; %w", err)
 	}
 	return nil
 }
 
 // Rejoin() - The player rejoins a table.
+// Note: Join, Rejoin and Quit are not thread-safe.
+// Do not call them from different go routines in the same instance of PLayer.
 func (p *Player) Rejoin(ctx context.Context) error {
 	secret, err := p.rejoin(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to rejoin; %w", err)
 	}
 	err = p.wsConn(ctx, secret)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to establish ws connection; %w", err)
 	}
 	return nil
 }
@@ -213,6 +217,8 @@ type quitTableRequest struct {
 }
 
 // Quit() - The player quits the table.
+// Note: Join, Rejoin and Quit are not thread-safe.
+// Do not call them from different go routines in the same instance of PLayer.
 func (p *Player) Quit(ctx context.Context) error {
 	s := p.ss
 	url := s.url.JoinPath("tables", p.Table.Summary.TableGUID, "quit")
@@ -237,6 +243,11 @@ func (p *Player) Quit(ctx context.Context) error {
 		return fmt.Errorf("wrong return code %v", resp.StatusCode)
 	}
 
+	if p.cli != nil {
+		p.cli.dispose()
+		p.cli = nil
+	}
+
 	return nil
 }
 
@@ -244,8 +255,13 @@ func (p *Player) Quit(ctx context.Context) error {
 // The function blocs until a message is received or when the context is canceled.
 func (p *Player) GetMsg(ctx context.Context) (json.RawMessage, error) {
 	select {
-	case msg := <-p.cli.inbox:
-		return msg, nil
+	case msg, ok := <-p.inbox:
+		if !ok {
+			log.Infof("inbox chan closed, exiting")
+			return nil, nil
+		}
+		p.turnId = msg.TurnID
+		return msg.Payload, nil
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
@@ -253,7 +269,15 @@ func (p *Player) GetMsg(ctx context.Context) (json.RawMessage, error) {
 
 // SendMsg() sends messages to the gamebox service.
 func (p *Player) SendMsg(ctx context.Context, msg json.RawMessage) error {
-	p.ss.
-		c.ws.outbox <- msg
+	mp := msgPlayer{
+		Payload:    msg,
+		PlayerGUID: p.Guid,
+		TurnID:     p.turnId,
+	}
+	select {
+	case p.outbox <- mp:
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 	return nil
 }
